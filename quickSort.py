e2473c5293a3dcf05253152086f72b1a4b5e9f28
Danial Cossette <m3ssee@gmail.com>
2023-11-23 17:33:24 -0500

    Changed comments.

--- quickSort.py
+++ quickSort.py
@@ -1,27 +1,22 @@
-# This method of sorting involves the splitting of elements based on a pivot element.
-# The split halves are then sorted recursively, with the final result putting the pivot
-# in it's final sorted position.
+# Quicksort sorts an array of elements by separating them based on a pivot element (greater than or less than pivot). The separated
+# halves are then sorted recursively and stored as a varaible. Finally, we return the sorted halves with the pivot in the middle.
+
 
 def quickSort(arr):
     if len(arr) < 2: # If the array has one or zero elements, it's already sorted.
         return arr
     
-    pivot = arr[-1] # Make any element a pivot, preferably the right most element.
-    less_than_pivot = [] # For elements less than pivot.
-    greater_than_pivot = [] # For elements greater than pivot.
+    pivot = arr[-1] # Make any element a pivot
+    less_than_pivot = [] 
+    greater_than_pivot = [] 
     
     for item in arr[:-1]: # Iterate over everything except the pivot.
         if item < pivot: 
             less_than_pivot.append(item)
         else:
             greater_than_pivot.append(item) # Separates the elements according to pivot.
-    
-    # Recursively sort the two split halves.
             
     less_than_sorted = quickSort(less_than_pivot)
-    greater_than_sorted = quickSort(greater_than_pivot)
+    greater_than_sorted = quickSort(greater_than_pivot) # Recursively sort the two split halves and store them.
     
-    # Final sorted position:
-    # The pivot will be after the elements less than it, and before the elements greater than it.
-    
-    return less_than_sorted + [pivot] + greater_than_sorted
+    return less_than_sorted + [pivot] + greater_than_sorted # The pivot will be in between the elements less than it and the elements greater than it. 
